0e3952d4bba04dfacedc208cc1c628bcbf3b6ffd
jisshub <jissmon476@gmail.com>
2020-05-18 21:29:08 +0530

    logout page ok

--- userApp/urls.py
+++ userApp/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as log_views
 urlpatterns = [
     path("", views.index_page, name="index"),
     path("register/", views.register_view, name="register"),
-    path("login/", log_views.LoginView.as_view(template_name="userApp/login.html"), name="login")
+    path("login/", log_views.LoginView.as_view(template_name="userApp/login.html"), name="login"),
+    path("logout/", log_views.LogoutView.as_view(template_name="userApp/logout.html"), name="logout"),
 ]
 
