8919f660104ed7f072c68a1eeae7b26f367e1c72
jisshub <jissmon476@gmail.com>
2020-05-18 16:17:36 +0530

    register.html - register form - user

--- userApp/urls.py
+++ userApp/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as log_views
 
 urlpatterns = [
-    path("", views.index_page, name="index")
+    path("", views.index_page, name="index"),
+    path("login/", log_views.LoginView.as_view(template_name="user/login.html"), name="login")
 ]
